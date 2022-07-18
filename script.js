aa22b46e104c0e0c934bb2f30e8a9d70dcf6900f
kishan-patel1 <kishanrpatel99@gmail.com>
2022-07-18 10:34:23 -0500

    added new releases module, added navbar to all modules

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
-let macBookImageElement = $(".macBook-image")
-let m2_4 = false;
-$(document).ready(function(){
-    $("img").on("click", function() {
-        // Change src attribute of image
-        if (! m2_4) {
-            $(this).attr("src", "assets/macBook_m2_4.webp");
-            $(this)
-                .css("width", "720px")
-                .css("height", "720px");
-            m2_4 = true;
-        }
-        else {
-            $(this).attr("src", "assets/macBook_m2_1.jpeg");
-            m2_4 = false;
-        }
-        
-    });    
-});
+function changeMacImage() {
+    let macBookImageElement = $(".macBook-image")
+    let m2_4 = false;
+    $(document).ready(function(){
+        $("img").on("click", function() {
+            // Change src attribute of image
+            if (! m2_4) {
+                $(this).attr("src", "assets/macBook_m2_4.webp");
+                $(this)
+                    .css("width", "723.2px")
+                    .css("height", "672px");
+                m2_4 = true;
+            }
+            else {
+                $(this).attr("src", "assets/macBook_m2_1.jpeg");
+                m2_4 = false;
+            }
+            
+        });    
+    });
+}
+changeMacImage();
